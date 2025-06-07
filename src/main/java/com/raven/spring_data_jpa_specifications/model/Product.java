66006e2d7a70e58caa228341ea27bf39f1c549f1
arijit83sarkar <mail2arijit83@gmail.com>
2025-06-08 01:31:58 +0530

    API for Product save.

--- src/main/java/com/raven/spring_data_jpa_specifications/model/Product.java
+++ src/main/java/com/raven/spring_data_jpa_specifications/model/Product.java
@@ -9,12 +9,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Builder
-@Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@Setter
-@ToString
 public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,4 +27,84 @@ public class Product implements Serializable {
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Review> reviews = new HashSet<>();
+
+    public Product() {
+    }
+
+    public Product(String name, String description, double price, boolean available, String manufacturer) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.available = available;
+        this.manufacturer = manufacturer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Set<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(Set<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", available=" + available +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", reviews=" + reviews +
+                '}';
+    }
 }
